99640e99f1aa5e7299705ff53a7aaf3bc9f4393f
Alin Apostol <alin.cristian977@gmail.com>
2022-02-23 20:18:56 +0200

    getting products data from api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'eCommerce';
+  products: any[];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: (n:any) => (this.products = n.data),
+      error: (e:any) => (console.log(e))
+    })
+  }
 }
