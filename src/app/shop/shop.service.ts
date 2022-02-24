cb18375f0fd187593ddebc8d2f0a317cefb1f3b8
Alin Apostol <alin.cristian977@gmail.com>
2022-02-24 09:47:13 +0200

    returning more than 6 products via query string

--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -12,6 +12,6 @@ export class ShopService {
   }
 
   getProducts() {
-    return this.http.get<IPagination>(this.baseUrl + 'products');
+    return this.http.get<IPagination>(this.baseUrl + 'products?pageSize=50');
   }
 }
