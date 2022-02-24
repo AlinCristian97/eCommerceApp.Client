d1419e99b41e0b04147ba596698c87267f3e3afd
Alin Apostol <alin.cristian977@gmail.com>
2022-02-24 02:05:04 +0200

    refactored NavBarComponent declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,18 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { NavBarComponent } from './core/nav-bar/nav-bar.component';
 import {HttpClientModule} from "@angular/common/http";
+import {CoreModule} from "./core/core.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
