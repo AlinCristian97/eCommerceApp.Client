dac762da30cb0fc6bedd2d3a64faf2322cb22421
Alin Apostol <alin.cristian977@gmail.com>
2022-02-24 09:40:40 +0200

    deleted products fetching from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {IPagination} from "./shared/models/pagination";
 
 @Component({
   selector: 'app-root',
@@ -9,15 +7,11 @@ import {IPagination} from "./shared/models/pagination";
 })
 export class AppComponent implements OnInit {
   title = 'eCommerce';
-  products: any[];
 
-  constructor(private http: HttpClient) {
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe({
-      next: (n:IPagination) => (this.products = n.data),
-      error: (e:any) => (console.log(e))
-    })
   }
+
 }
