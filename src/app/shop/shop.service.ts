b137f32f655fa984a0af6932e5dda4ba249b511a
Alin Apostol <alin.cristian977@gmail.com>
2022-02-26 02:07:15 +0200

    implemented get method for brands

--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {IPagination} from "../shared/models/pagination";
+import {IBrand} from "../shared/models/brand";
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,9 @@ export class ShopService {
 
   getProducts() {
     return this.http.get<IPagination>(this.baseUrl + 'products?pageSize=50');
+  }
+
+  getBrands() {
+    return this.http.get<IBrand>(this.baseUrl + 'products/brands');
   }
 }
