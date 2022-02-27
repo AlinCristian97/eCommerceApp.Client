2651c128cca1ada10f7c9af003c408f9a8207a78
Alin Apostol <alin.cristian977@gmail.com>
2022-02-27 03:53:09 +0200

    added new module

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -75,8 +75,7 @@ export class ShopComponent implements OnInit {
   }
 
   onPageChanged(event: any) {
-    if (this.shopParams.pageNumber !== event)
-    {
+    if (this.shopParams.pageNumber !== event) {
       this.shopParams.pageNumber = event;
       this.getProducts();
     }
