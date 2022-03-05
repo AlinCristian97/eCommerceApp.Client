8b40dade4e32941f37ca3cc8ad4e9789860bdcf5
Alin Apostol <alin.cristian977@gmail.com>
2022-03-05 19:00:35 +0200

    using breadcrumb service

--- src/app/shop/product-details/product-details.component.ts
+++ src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {IProduct} from "../../shared/models/product";
 import {ShopService} from "../shop.service";
 import {ActivatedRoute} from "@angular/router";
+import {BreadcrumbService} from "xng-breadcrumb";
 
 @Component({
   selector: 'app-product-details',
@@ -11,7 +12,8 @@ import {ActivatedRoute} from "@angular/router";
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
 
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) { }
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute,
+              private breadcrumbService: BreadcrumbService) { }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -19,7 +21,10 @@ export class ProductDetailsComponent implements OnInit {
 
   loadProduct() {
     this.shopService.getProduct(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe({
-      next: (n) => (this.product = n),
+      next: (n) => {
+        this.product = n;
+        this.breadcrumbService.set('@productDetails', n.name);
+      },
       error: (e) => (console.log(e))
     });
   }
