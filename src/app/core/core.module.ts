bf09daf1145c4592b3548a3044495c1edd8f868f
Alin Apostol <alin.cristian977@gmail.com>
2022-03-05 15:23:09 +0200

    adding toast notifications

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,6 +5,7 @@ import {RouterModule} from "@angular/router";
 import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
+import {ToastrModule} from "ngx-toastr";
 
 
 
@@ -15,10 +16,14 @@ import { ServerErrorComponent } from './server-error/server-error.component';
     NotFoundComponent,
     ServerErrorComponent
   ],
-    imports: [
-        CommonModule,
-        RouterModule
-    ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true
+    })
+  ],
   exports: [
     NavBarComponent
   ]
